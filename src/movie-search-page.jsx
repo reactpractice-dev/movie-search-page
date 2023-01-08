fa7fcb753674c2d05607820c18a32b745c593cde
Corina Udrescu <corinutz@gmail.com>
2023-01-08 14:26:45 +0100

    Show the list of movies

--- src/movie-search-page.jsx
+++ src/movie-search-page.jsx
@@ -1,3 +1,32 @@
-const MovieSearchPage = () => <div>Build your movie search page here!</div>;
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const MovieSearchPage = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: "1234",
+          query: "amelie",
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Movie search page</h2>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.original_title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default MovieSearchPage;
