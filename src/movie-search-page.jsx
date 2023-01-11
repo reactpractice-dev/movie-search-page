9ba2a32aec1566dfc099306cb8f7f9cd74bfe9cb
Corina Udrescu <corinutz@gmail.com>
2023-01-11 08:20:30 +0100

    Add search input

--- src/movie-search-page.jsx
+++ src/movie-search-page.jsx
@@ -4,23 +4,34 @@ import Movie from "./movie";
 
 const MovieSearchPage = () => {
   const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  useEffect(() => {
+  const handleMovieSearch = (e) => {
+    e.preventDefault();
     axios
       .get("https://api.themoviedb.org/3/search/movie", {
         params: {
           api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
-          query: "amelie",
+          query: searchQuery,
         },
       })
       .then((response) => {
         setMovies(response.data.results);
       });
-  }, []);
+  };
 
   return (
     <div>
       <h2>Movie search page</h2>
+      <form onSubmit={handleMovieSearch} style={{ marginBottom: "30px" }}>
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {movies.map((movie) => (
           <Movie key={movie.id} movie={movie} />
