b229e2832cfea15a8036a94d6ceceaf8d353db4b
Corina Udrescu <corinutz@gmail.com>
2023-01-11 09:02:11 +0100

    Load smaller images

--- src/movie.jsx
+++ src/movie.jsx
@@ -6,7 +6,7 @@ const Movie = ({ movie }) => {
       <h3>{movie.original_title}</h3>
       {movie.poster_path && (
         <img
-          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
           style={{ width: "100px" }}
         />
       )}
